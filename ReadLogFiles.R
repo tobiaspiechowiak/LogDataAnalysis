91e77ff9af9ccabcdacd8bc1ebcda45eadd31890
Tobias Piechowiak <tpiechowiak@gnresound.com>
2019-06-14 17:12:01 +0200

    First commit

--- ReadLogFiles.R
+++ ReadLogFiles.R
@@ -6,12 +6,10 @@ dist <- 12
 linn <- readLines('AlgoBrowser3.log')
 for (i in 1:length(linn)){
   #extract data stamp
-  tmp <- linn[seq(3,length(linn),dist)] %>% Numextract() 
-  
+  tmp <- linn[3+(i-1)*12] %>% Numextract() 
+
 
 }
-
-
 
 
 
